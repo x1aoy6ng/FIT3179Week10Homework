34fc8c06125b833347734e76e388c6f353ab4cee
Yeo Yu Xuan <yeoyuxuan302@gmail.com>
2025-10-02 23:08:11 +0800

    Modified style.css to align both map and bar chart, filter the state to top 5 (barchart)

---
+++ W10Homework.js
@@ -0,0 +1,9 @@
+var vg_1 = "W9Homework.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "W10Homework.json";
+
+vegaEmbed("#barchart", vg_2).then(function(result) {
+}).catch(console.error);
